ef88166580f8a5e44664660d2dd4c2ad617ed86d
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-04-02 19:09:39 +0200

    Sync Movements

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -17,11 +17,12 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SerializeField] private Renderer playerRenderer=null;
     [SerializeField] private TMP_Text playerShowDisplayName=null;
 
+
+    #region server
     [Server]
     public void setDisplayName(string name) { 
         this.displayName = name;
     }
-
 
     [Server]
     public void setPlayerColor(Color color)
@@ -29,6 +30,27 @@ public class MyNetworkPlayer : NetworkBehaviour
         this.playerColor = color;
     }
 
+    [Command]
+    public void CmdSetDisplayName(string name) {
+
+        Debug.Log("lenght = " + name.Length);
+        if (name.Length < 3 || name.Length > 20) {
+            Debug.Log("Your name is too fucking stupid");
+            return; 
+        }
+
+        setDisplayName(name);
+        RpcSendName(name);
+    }
+
+    #endregion
+
+
+
+
+
+
+    #region client
     private void handleDisplayColor(Color oldColor, Color newColor) {
 
         playerRenderer.material.SetColor("_BaseColor", newColor);
@@ -36,7 +58,20 @@ public class MyNetworkPlayer : NetworkBehaviour
 
     private void handleDisplayName(string oldName, string newName)
     {
-
         playerShowDisplayName.text = newName;
     }
+
+    [ContextMenu("set Name")]
+    private void setMyName() {
+
+        CmdSetDisplayName("Yo");
+    }
+
+
+    [ClientRpc]
+    public void RpcSendName(string name)
+    {
+        Debug.Log("Your focking Name:" + name);
+    }
+    #endregion
 }
